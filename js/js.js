6e28b436d4b008e75c028dfc51477a31d2d59e64
Max Martin <maxmartin360@gmail.com>
2015-06-02 21:09:04 -0400

    initial commit

---
+++ js/js.js
@@ -0,0 +1,29 @@
+/*for (var i=1; i <= 20; i++)
+{
+    if (i % 15 == 0){
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 == 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log("Buzz");
+    }
+    else
+        console.log(i);
+}*/
+var myFunction = function(){
+	for (i=1; i <= 100; i++){
+    	if (i % 15 == 0){
+        $('#list-area').append('<li>FizzBuzz</li>');
+   	 }
+    	else if (i % 3 == 0) {
+        $('#list-area').append('<li>Fizz</li>');
+    }
+    	else if (i % 5 == 0) {
+        $('#list-area').append('<li>Buzz</li>');
+    }
+   		 else
+        $('#list-area').append('<li>' + (i) + '</li>');
+};
+}
